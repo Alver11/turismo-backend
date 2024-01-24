d91d45a53a37a2423649deed828cbd3bb262943d
tic-pima <alverromero11@gmail.com>
2024-01-24 01:59:41 -0300

    retornando datos y funciones para atributos

--- app/Http/Controllers/Api/V1/AttributeController.php
+++ app/Http/Controllers/Api/V1/AttributeController.php
@@ -5,13 +5,62 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Attribute;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Yajra\DataTables\Facades\DataTables;
 
 class AttributeController extends Controller
 {
+    public function index(): JsonResponse
+    {
+        $query = Attribute::query();
+        return DataTables::of($query)->toJson();
+    }
     public function getAttribute(): Collection|array
     {
         return Attribute::get();
     }
 
+    public function store(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|unique:roles|max:255',
+        ]);
+        Attribute::create(['name' => $validatedData['name'], 'type_input' =>'text']);
+        return response()->json(['message' => 'Atriburo creado exitosamente']);
+    }
+
+    public function show($id)
+    {
+        return Attribute::findOrFail($id);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $attribute = Attribute::findOrFail($id);
+        if (!$attribute) {
+            return response()->json(['message' => 'Atributo no encontrado'], 404);
+        }
+
+        $validatedData = $request->validate([
+            'name' => 'required|unique:roles,name,' . $id . '|max:255',
+        ]);
+        $attribute->name = $validatedData['name'];
+        $attribute->save();
+
+        return response()->json(['message' => 'Atributo actualizado exitosamente']);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        try {
+            $attribute = Attribute::findOrFail($id);
+            $attribute->delete();
+            return response()->json(['message' => 'Eliminado con éxito']);
+        } catch (QueryException $e) {
+            return response()->json(['message' => 'Error al Eliminar el Atributo']);
+        }
+    }
 }
--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -9,10 +9,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 /**
  * @method static insert(array[] $data)
  * @method static get()
+ * @method static create(array $array)
+ * @method static findOrFail($id)
+ * @method static findById($id)
  */
 class Attribute extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'icon',
+        'type_input',
+    ];
 
     public function touristPlaces(): BelongsToMany
     {
