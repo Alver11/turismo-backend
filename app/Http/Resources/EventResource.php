46d44e8c1a0819d07648513a938918a428243ed2
tic-pima <alverromero11@gmail.com>
2024-10-12 16:52:47 -0300

    agregando fecha al retorno de los eventos

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use mysql_xdevapi\Collection;
 
 class EventResource extends JsonResource
 {
