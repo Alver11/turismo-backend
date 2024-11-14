9820d03cc2c39108613d1da6c87949689e1df881
tic-pima <alverromero11@gmail.com>
2024-11-14 02:45:45 -0300

    ajuste de jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new TrainOpenAIModelJob)->weeklyOn(1, '00:00');
+        $schedule->job(new TrainOpenAIModelJob)->everyTwoMinutes();
+        // $schedule->job(new TrainOpenAIModelJob)->weeklyOn(1, '00:00');
     }
 
     /**
